9008c616445d5337fe1cca0f13a38f32ede0725d
kayhub-eco <kayhub@kayhub.com>
2025-09-10 14:34:51 +0800

    Add files via upload

---
+++ config.php
@@ -0,0 +1,17 @@
+<?php
+
+  global $config;
+  $config = [];
+
+  // 程序安装路径
+  $config['path'] = '/';
+  // ID 长度
+  $config['length'] = 8;
+  // 网站标题
+  $config['title'] = 'KayHub生态软件-短链生成';
+  // 网站简介
+  $config['description'] = 'KayHub生态软件在线网址缩短服务';
+  
+  $config['api'] = true;
+
+?>
